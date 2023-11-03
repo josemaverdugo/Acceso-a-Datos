fd662edcf28df47ce53fe00492c23cdae05b1c00
josemaverdugo <123060599+josemaverdugo@users.noreply.github.com>
2023-11-03 16:04:46 +0100

    Añadido Modificar y Eliminar Alumnos

--- Prueba2/src/main/java/Prueba2/Prueba2/ControllerTest.java
+++ Prueba2/src/main/java/Prueba2/Prueba2/ControllerTest.java
@@ -6,6 +6,7 @@ package Prueba2.Prueba2;
 
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +14,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -39,5 +41,34 @@ public class ControllerTest {
     public String form(Model model){
         model.addAttribute("alumno", new Alumno());
         return "formulario";
+    }
+    @GetMapping("/eliminar/{id}")
+    public String eliminarAlumno(@PathVariable Integer id) {
+        alumnorepository.deleteById(id);
+        return "redirect:/Inicio";
+    }
+
+    @PostMapping("/editar/" + "{id}")
+            public String update(int id, Alumno a) {
+    //public String update(@PathVariable int id, @ModelAttribute Alumno alumnoActualizado) {
+        Optional<Alumno> alumnoExistente = alumnorepository.findById(id);
+        //alumnoExistente.ifPresent(value->alumnorepository.save(value));
+        
+        if (alumnoExistente.isPresent()) {
+            Alumno alumno = alumnoExistente.get();
+            alumno.setNombre(a.getNombre());
+            alumno.setApellido(a.getApellido());
+            alumno.setEdad(a.getEdad());
+            alumno.setDni(a.getDni());
+            alumnorepository.save(alumno);
+        }
+        return "redirect:/Inicio";
+    }
+
+    @GetMapping("/editar/" + "{id}")
+    public String update(@PathVariable Integer id, Model model) {
+        Optional<Alumno> alumno = alumnorepository.findById(id);
+        model.addAttribute("alumno", alumno);
+        return "modificar";
     }
 }
